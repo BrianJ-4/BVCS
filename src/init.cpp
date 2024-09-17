1c2ff17f803157211d36caf93fdebb44484a6f1b
BrianJ-4 <brian.joshua03@gmail.com>
2024-09-16 22:59:48 -0700

    Initial init.cpp file created

--- src/init.cpp
+++ src/init.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
 
-void init()
+void init(const std::string& name)
 {
-    std::cout << "INIT HIT" << std::endl;
+    if (std::filesystem::exists(name))
+    {
+        std::cout << "Repository already exists in this location" << std::endl;
+        return;
+    }
+
+    std::filesystem::create_directory(name);
+    std::filesystem::create_directory(name + "/.bvcs");
+    std::filesystem::create_directory(name + "/.bvcs/contents");
+    std::filesystem::create_directory(name + "/.bvcs/refs");
+    std::filesystem::create_directory(name + "/.bvcs/refs/heads");
+
+    std::ofstream headFile(name + "/.bvcs/HEAD");
+    headFile << "ref: refs/heads/main\n";
+    headFile.close();
+
+    std::ofstream masterFile(name + "/.bvcs/refs/heads/main");
+    masterFile.close();
+
+    std::cout << "Initialized new BVCS repository " << name << std::endl;
 }
