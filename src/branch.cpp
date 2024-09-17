9de0517cc7ef7ba4ebe01dcb8f294af49c7da0ef
BrianJ-4 <brian.joshua03@gmail.com>
2024-09-17 16:37:56 -0700

    Initial implementation of branch. Allows user to create new branch

--- src/branch.cpp
+++ src/branch.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+namespace fs = std::filesystem;
 
 void branch(const std::string& branchName)
 {
-    std::cout << "BRANCH HIT" << std::endl;
+    // Check if repo has been created
+    fs::path repoPath = {".bvcs"}; 
+    if (!fs::exists(repoPath))
+    {
+        std::cout << "Initialize repo first" << std::endl;
+        return;
+    }
+
+    fs::path newBranchPath = ".bvcs/refs/heads/" + branchName;
+    if (fs::exists(newBranchPath))
+    {
+        std::cout << "Branch already exists" << std::endl;
+        return;
+    }
+    
+    std::ofstream newBranch(newBranchPath);
+    newBranch.close();
+    std::cout << "Created branch " << branchName << std::endl;
 }
