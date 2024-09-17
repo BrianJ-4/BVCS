cea86d525a71c75e83736a02177467a759904c9b
BrianJ-4 <brian.joshua03@gmail.com>
2024-09-16 23:47:24 -0700

    Initial start to staging logic

--- src/stage.cpp
+++ src/stage.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <filesystem>
 
-void stage(const std::string& file)
+void stage(const std::string& repo, const std::string& file)
 {
-    std::cout << "STAGE HIT" << std::endl;
+    // Check if the repo has been created
+    std::filesystem::path repoParentPath = repo;
+    std::filesystem::path repoPath = repoParentPath / ".bvcs"; 
+    if (!std::filesystem::exists(repoPath))
+    {
+        std::cout << "Initialize " << repo << " repo first" << std::endl;
+        return;
+    }
+
+    if (!std::filesystem::exists(file))
+    {
+        std::cout << "File not found" << std::endl;
+        return;
+    }
+
+    std::filesystem::path stagingPath = repoPath / "staging";
+    std::filesystem::path filePath = repoParentPath / file;
 }
