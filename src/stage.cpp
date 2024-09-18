6ec57fd5f4ddcc9e99e373c9962d7c6f29b9918b
BrianJ-4 <brian.joshua03@gmail.com>
2024-09-17 20:30:01 -0700

    Implemented initial staging logic

--- src/stage.cpp
+++ src/stage.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <filesystem>
+using namespace std;
 namespace fs = std::filesystem;
 
 void stage(const std::string& file)
@@ -18,6 +19,15 @@ void stage(const std::string& file)
         std::cout << "File not found" << std::endl;
         return;
     }
-
     
+    fs::path filePath(file);                            // Path of file
+    fs::path stagingPath = ".bvcs/staging";             // Staging path in repo
+    fs::path stagingFilePath = stagingPath / filePath;  // Final path where file should be saved
+
+    // Create the directory where we will store the file and its path
+    // Then copy the file, making sure to overwrite in case file has already been staged
+    fs::create_directory(stagingFilePath.parent_path());
+    fs::copy(filePath, stagingFilePath, fs::copy_options::overwrite_existing);
+
+    cout << "Staged " << file << endl;
 }
