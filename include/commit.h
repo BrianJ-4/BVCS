3946cb2b027ea688522b243cd926550b2468a572
BrianJ-4 <brian.joshua03@gmail.com>
2024-09-19 14:36:11 -0700

    Moved time string generation to its own method

--- include/commit.h
+++ include/commit.h
@@ -4,6 +4,7 @@
 #include <string>
 
 void commit(const std::vector<std::string>& messageVector);
-std::string generateCommitID(const std::string& message);
+std::string generateCommitID(const std::string& message, const std::string& time);
+std::string getSystemTime();
 
 #endif
